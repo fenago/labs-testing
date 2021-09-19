bd70799efc518410d4e3daa6beb58fb6055358fe
fenago <socrates73@gmail.com>
2021-09-20 00:11:23 +0500

    updated

--- Lab02/HelloWorldStruts2-MultipleActions/src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ Lab02/HelloWorldStruts2-MultipleActions/src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -23,3 +23,19 @@ public class HelloWorldAction extends ActionSupport {
       this.name = name;
    }
 }
+
+public class SomeOtherClass extends ActionSupport {
+   private String name;
+
+   public String execute() {
+      return MyAction.GOOD;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+}
--- Lab03/HelloWorldStruts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ Lab03/HelloWorldStruts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -1,17 +1,20 @@
 package com.tutorialspoint.struts2;
 
-public class HelloWorldAction {
-	   private String name;
+import com.opensymphony.xwork2.ActionSupport;
 
-	   public String execute() throws Exception {
-	      return "success";
-	   }
-	   
-	   public String getName() {
-	      return name;
-	   }
+public class HelloWorldAction extends ActionSupport {
+   private String name;
 
-	   public void setName(String name) {
-	      this.name = name;
-	   }
-	}
+   public String execute() throws Exception {
+      System.out.println("Inside action....");
+      return "success";
+   }  
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+}
