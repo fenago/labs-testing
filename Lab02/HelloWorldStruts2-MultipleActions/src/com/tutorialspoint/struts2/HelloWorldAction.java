abc4afdb27d4cd8203b766888dad200bf54712c9
fenago <socrates73@gmail.com>
2021-09-20 00:03:38 +0500

    updated

--- Lab02/HelloWorldStruts2-MultipleActions/src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ Lab02/HelloWorldStruts2-MultipleActions/src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -2,15 +2,17 @@ package com.tutorialspoint.struts2;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+class MyAction extends ActionSupport {
+   public static String GOOD = SUCCESS;
+   public static String BAD = ERROR;
+}
+
 public class HelloWorldAction extends ActionSupport {
    private String name;
 
-   public String execute() throws Exception {
-      if ("SECRET".equals(name)) {
-         return SUCCESS;
-      } else {
-         return ERROR;  
-      }
+   public String execute() {
+      if ("SECRET".equals(name)) return MyAction.GOOD;
+      return MyAction.BAD;
    }
    
    public String getName() {
